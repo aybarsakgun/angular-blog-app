15da3883e3dcb54dbd97a219eeafce8fb1156bf5
Aybars AKGÜN <aybars.akgun77@gmail.com>
2021-06-25 02:34:43 +0300

    User list code refactored.

---
+++ src/app/shared/services/user-list.service.ts
@@ -0,0 +1,18 @@
+import {Injectable} from "@angular/core";
+import {BehaviorSubject} from "rxjs";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserListService {
+  public currentPage$: BehaviorSubject<number> = new BehaviorSubject<number>(1);
+  public nameFilter$: BehaviorSubject<string> = new BehaviorSubject<string>('');
+
+  changePage(page: number): void {
+    this.currentPage$.next(page);
+  }
+
+  changeNameFilter(name: string): void {
+    this.nameFilter$.next(name);
+  }
+}
